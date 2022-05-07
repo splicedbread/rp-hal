85cbd66763421bc013490df35cd5e44757dde872
splicedbread <dxbunrated@gmail.com>
2022-05-07 16:42:12 -0700

    Updated boot parameters in lib.rs

--- boards/arduino_nano_connect/src/lib.rs
+++ boards/arduino_nano_connect/src/lib.rs
@@ -13,7 +13,7 @@ pub use cortex_m_rt::entry;
 #[link_section = ".boot2"]
 #[no_mangle]
 #[used]
-pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_AT25SF128A;
 
 use embedded_hal::{
     adc::{Channel, OneShot},
